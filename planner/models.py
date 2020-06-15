a8e8622a2816f3301c315e73691ae728c4bc26f8
Blessing Sibanda <banQomania@users.noreply.github.com>
2020-06-15 10:26:23 +0200

    add camera and SurveyType models

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# Camera model.
+class Camera(models.Model):
+    camera_type = models.CharField(max_length=200)
+    sensorw = models.DecimalField()           
+    sensorh = models.DecimalField()
+    pixelw  = models.IntegerField()
+    pixelh = models.IntegerField()
+    mpixel = models.DecimalField() 
+    lensFocal = models.DecimalField() 
+    imageSize = models.DecimalField() 
+    comments = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.camera_type
+
+    
+class SurveyType(models.Model):
+    name = models.CharField(max_length=50)
+    forward = models.IntegerField()
+    lateral = models.IntegerField()
+
+    def __str__(self):
+        return self.name
