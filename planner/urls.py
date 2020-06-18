f39a26b93609a085e5eb3ebbcdf142892ab6e91b
codeplayer12 <noblemutoko@gmail.com>
2020-06-18 17:49:08 +0200

    customize the admin interface

--- planner/urls.py
+++ planner/urls.py
@@ -1,6 +1,12 @@
+from django.contrib import admin
 from django.urls import path
 
 from . import views
+
+
+admin.site.site_header = "Survery Planner Admin"
+admin.site.site_title = "Survey Planner | Budget Calculator"
+admin.site.index_title = "Survey Planner"
 
 urlpatterns = [
     # path('', views.index, name='index'),
