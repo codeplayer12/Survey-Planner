3e465352de4a5bf6d2ee4ce2e33b1667c5dfd21f
codeplayer12 <noblemutoko@gmail.com>
2020-06-25 18:46:48 +0200

    new local settings

--- survey_planner/settings.py
+++ survey_planner/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production")
