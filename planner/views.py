83ca1b9de670dba61417b4aeae074fb334635c6e
Blessing Sibanda <banQomania@users.noreply.github.com>
2020-07-01 13:35:43 +0200

    add new models and run migrations
    Co-authored-by: noble mutoko <noblemutoko@gmail.com>

--- planner/views.py
+++ planner/views.py
@@ -12,32 +12,32 @@ import requests
 def index(request):
 
     if request.method == "POST":
-       drone_id = request.POST['drone']
-       camera_id = request.POST['camera']
-       survey_type_id = request.POST['survey_select']
-       take_of_area_distance = int(request.POST['take_of_area'])
-       area_size  = area_calc(int(request.POST['area_size']),request.POST['units'])
-       bttry_capacity = int(request.POST['battery_capacity'])
-       flight_height = int(request.POST['flight_height'])
+        drone_id = request.POST['drone']
+        camera_id = request.POST['camera']
+        survey_type_id = request.POST['survey_select']
+        take_of_area_distance = int(request.POST['take_of_area'])
+        area_size  = area_calc(int(request.POST['area_size']),request.POST['units'])
+        bttry_capacity = int(request.POST['battery_capacity'])
+        flight_height = int(request.POST['flight_height'])
 
-       selected_drone = Drone.objects.get(id = drone_id)
-       distance_travelled_per_flight = (selected_drone.flightTime * 60 * selected_drone.cruiseSpeed)/(1+ (bttry_capacity/100) + flight_height *(0.01/12.5))
-       cal = Calculations(camera_id,survey_type_id,bttry_capacity,flight_height,take_of_area_distance,area_size,distance_travelled_per_flight)
-       planner_values = json.loads(cal.get_planner_display_values())   
+        selected_drone = Drone.objects.get(id = drone_id)
+        distance_travelled_per_flight = (selected_drone.flightTime * 60 * selected_drone.cruiseSpeed)/(1+ (bttry_capacity/100) + flight_height *(0.01/12.5))
+        cal = Calculations(camera_id,survey_type_id,bttry_capacity,flight_height,take_of_area_distance,area_size,distance_travelled_per_flight)
+        planner_values = json.loads(cal.get_planner_display_values())   
 
-       cameras = Camera.objects.all()
-       drones = Drone.objects.all()
-       surveys = SurveyType.objects.all() 
+        cameras = Camera.objects.all()
+        drones = Drone.objects.all()
+        surveys = SurveyType.objects.all() 
 
     #    print("Camera id "+str(camera_id))
     #    print(planner_values)
 
-       select_survey = surveys.get(id = survey_type_id)
-       select_drone = drones.get(id=drone_id)
-       select_camera = cameras.get(id=camera_id)
-       return render(request, 'planner/index.html',
-       {'planner_values': planner_values,'cameras':cameras,'drones':drones,'surveys':surveys,'select_drone':select_drone,
-       'select_survey' :select_survey,'select_camera':select_camera}) 
+        select_survey = surveys.get(id = survey_type_id)
+        select_drone = drones.get(id=drone_id)
+        select_camera = cameras.get(id=camera_id)
+        return render(request, 'planner/index.html',
+        {'planner_values': planner_values, 'cameras':cameras,'drones':drones,'surveys':surveys,'select_drone':select_drone,
+        'select_survey' :select_survey,'select_camera':select_camera}) 
     # except Exception as e:
     #     return HttpResponse( str(e))
 
@@ -70,7 +70,7 @@ def index(request):
         return HttpResponse( str(e))
 
 def area_calc(area,unit):
-    if unit =="kilometres":
+    if unit == "kilometres":
         return area*1000000
     elif unit == "hectares":
         return area * 100
